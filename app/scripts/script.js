09e4dd01cc16461e549313ef96727673507b0f9d
Ramon Medeiro <r.medeiro10@gmail.com>
2024-09-07 13:33:48 -0300

    organizando repositório

--- app/scripts/script.js
+++
@@ -1,8 +0,0 @@
-document.querySelector('.search-input').addEventListener('input', function() {
-    const label = document.querySelector('.search-label');
-    if (this.value !== '') {
-        label.classList.add('filled');
-    } else {
-        label.classList.remove('filled');
-    }
-});
